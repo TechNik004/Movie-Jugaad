c3c81b67f0e49b358fdc57f4dae9289ba4ef5d87
Nikhil792 <114512871+Nikhil792@users.noreply.github.com>
2025-02-28 21:43:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,7 +88,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/NewMoviesOO')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Thorrequest')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/TrexOfficialss')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Trexowner')
@@ -125,9 +125,9 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
-SHORTLINK_API = environ.get('SHORTLINK_API', '')
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'publicearn.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '3799ed9499671191f206f84272385f68a61f82e7')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/newewwwwwwww/10') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
