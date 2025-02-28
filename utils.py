d5e6b19090a00f1d09d3c07d88553ce2899ed44e
Nikhil792 <114512871+Nikhil792@users.noreply.github.com>
2025-02-28 19:21:53 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -598,57 +598,57 @@ async def check_verification(bot, userid):
     else:
         return False  
     
-#async def send_all(bot, userid, files, ident, chat_id, user_name, query):
-    #settings = await get_settings(chat_id)
-   # if 'is_shortlink' in settings.keys():
-      #  ENABLE_SHORTLINK = settings['is_shortlink']
- #   else:
-    #    await save_group_settings(message.chat.id, 'is_shortlink', False)
-     #   ENABLE_SHORTLINK = False
-  #  try:
-   #     if ENABLE_SHORTLINK:
-    #        for file in files:
-      #          title = file["file_name"]
-      #          size = get_size(file["file_size"])
-      #          if not await db.has_premium_access(userid) and SHORTLINK_MODE == True:
-       #             await bot.send_message(chat_id=userid, text=f"<b>Hᴇʏ ᴛʜᴇʀᴇ {user_name} 👋🏽 \n\n✅ Sᴇᴄᴜʀᴇ ʟɪɴᴋ ᴛᴏ ʏᴏᴜʀ ғɪʟᴇ ʜᴀs sᴜᴄᴄᴇssғᴜʟʟʏ ʙᴇᴇɴ ɢᴇɴᴇʀᴀᴛᴇᴅ ᴘʟᴇᴀsᴇ ᴄʟɪᴄᴋ ᴅᴏᴡɴʟᴏᴀᴅ ʙᴜᴛᴛᴏɴ\n\n🗃️ Fɪʟᴇ Nᴀᴍᴇ : {title}\n🔖 Fɪʟᴇ Sɪᴢᴇ : {size}</b>", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("📤 Dᴏᴡɴʟᴏᴀᴅ 📥", url=await get_shortlink(chat_id, f"https://telegram.me/{temp.U_NAME}?start=files_{file['file_id']}"))]]))
-     #   else:
-     #       for file in files:
-     #           f_caption = file["caption"]
-     #           title = file["file_name"]
-      #          size = get_size(file["file_size"])
-        #        if CUSTOM_FILE_CAPTION:
-           #         try:
-           #             f_caption = CUSTOM_FILE_CAPTION.format(
-           #                 file_name='' if title is None else title,
-           #                 file_size='' if size is None else size,
-           #                 file_caption='' if f_caption is None else f_caption
-          #              )
-                   # except Exception as e:
-                   #     print(e)
-                  #      f_caption = f_caption
-              #  if f_caption is None:
-             #       f_caption = f"{title}"
-             #   await bot.send_cached_media(
-             #       chat_id=userid,
-             #       file_id=file["file_id"],
-              #      caption=f_caption,
-              #      protect_content=True if ident == "filep" else False,
-               #     reply_markup=InlineKeyboardMarkup(
-                #        [[
-               #             InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
-                #            InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
-                #        ],[
-                  #          InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url=OWNER_LNK)
-                #        ]]
-                 #   )
-                #)
-    #except UserIsBlocked:
-      #  await query.answer('Uɴʙʟᴏᴄᴋ ᴛʜᴇ ʙᴏᴛ ᴍᴀʜɴ !', show_alert=True)
-    #except PeerIdInvalid:
-     #   await query.answer('Hᴇʏ, Sᴛᴀʀᴛ Bᴏᴛ Fɪʀsᴛ Aɴᴅ Cʟɪᴄᴋ Sᴇɴᴅ Aʟʟ', show_alert=True)
- #   except Exception as e:
-      #  await query.answer('Hᴇʏ, Sᴛᴀʀᴛ Bᴏᴛ Fɪʀsᴛ Aɴᴅ Cʟɪᴄᴋ Sᴇɴᴅ Aʟʟ', show_alert=True)
+async def send_all(bot, userid, files, ident, chat_id, user_name, query):
+    settings = await get_settings(chat_id)
+    if 'is_shortlink' in settings.keys():
+        ENABLE_SHORTLINK = settings['is_shortlink']
+    else:
+        await save_group_settings(message.chat.id, 'is_shortlink', False)
+        ENABLE_SHORTLINK = False
+    try:
+        if ENABLE_SHORTLINK:
+            for file in files:
+                title = file["file_name"]
+                size = get_size(file["file_size"])
+                if not await db.has_premium_access(userid) and SHORTLINK_MODE == True:
+                    await bot.send_message(chat_id=userid, text=f"<b>Hᴇʏ ᴛʜᴇʀᴇ {user_name} 👋🏽 \n\n✅ Sᴇᴄᴜʀᴇ ʟɪɴᴋ ᴛᴏ ʏᴏᴜʀ ғɪʟᴇ ʜᴀs sᴜᴄᴄᴇssғᴜʟʟʏ ʙᴇᴇɴ ɢᴇɴᴇʀᴀᴛᴇᴅ ᴘʟᴇᴀsᴇ ᴄʟɪᴄᴋ ᴅᴏᴡɴʟᴏᴀᴅ ʙᴜᴛᴛᴏɴ\n\n🗃️ Fɪʟᴇ Nᴀᴍᴇ : {title}\n🔖 Fɪʟᴇ Sɪᴢᴇ : {size}</b>", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("📤 Dᴏᴡɴʟᴏᴀᴅ 📥", url=await get_shortlink(chat_id, f"https://telegram.me/{temp.U_NAME}?start=files_{file['file_id']}"))]]))
+        else:
+            for file in files:
+                f_caption = file["caption"]
+                title = file["file_name"]
+                size = get_size(file["file_size"])
+                if CUSTOM_FILE_CAPTION:
+                    try:
+                        f_caption = CUSTOM_FILE_CAPTION.format(
+                        file_name='' if title is None else title,
+                            file_size='' if size is None else size,
+                            file_caption='' if f_caption is None else f_caption
+                        )
+                    except Exception as e:
+                        print(e)
+                        f_caption = f_caption
+              if f_caption is None:
+                    f_caption = f"{title}"
+                await bot.send_cached_media(
+                    chat_id=userid,
+                    file_id=file["file_id"],
+                    caption=f_caption,
+                    protect_content=True if ident == "filep" else False,
+                    reply_markup=InlineKeyboardMarkup(
+                        [[
+                            InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
+                            InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
+                        ],[
+                            InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url=OWNER_LNK)
+                        ]]
+                    )
+                )
+    except UserIsBlocked:
+        await query.answer('Uɴʙʟᴏᴄᴋ ᴛʜᴇ ʙᴏᴛ ᴍᴀʜɴ !', show_alert=True)
+    except PeerIdInvalid:
+        await query.answer('Hᴇʏ, Sᴛᴀʀᴛ Bᴏᴛ Fɪʀsᴛ Aɴᴅ Cʟɪᴄᴋ Sᴇɴᴅ Aʟʟ', show_alert=True)
+    except Exception as e:
+        await query.answer('Hᴇʏ, Sᴛᴀʀᴛ Bᴏᴛ Fɪʀsᴛ Aɴᴅ Cʟɪᴄᴋ Sᴇɴᴅ Aʟʟ', show_alert=True)
         
 async def get_cap(settings, remaining_seconds, files, query, total_results, search):
     if settings["imdb"]:
