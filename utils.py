55973468993daaf84d556245b1b74d06b24477bc
Nikhil792 <114512871+Nikhil792@users.noreply.github.com>
2025-02-28 20:05:36 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -620,14 +620,14 @@ async def send_all(bot, userid, files, ident, chat_id, user_name, query):
                 if CUSTOM_FILE_CAPTION:
                     try:
                         f_caption = CUSTOM_FILE_CAPTION.format(
-                        file_name='' if title is None else title,
+                            file_name='' if title is None else title,
                             file_size='' if size is None else size,
                             file_caption='' if f_caption is None else f_caption
                         )
                     except Exception as e:
                         print(e)
                         f_caption = f_caption
-              if f_caption is None:
+                if f_caption is None:
                     f_caption = f"{title}"
                 await bot.send_cached_media(
                     chat_id=userid,
