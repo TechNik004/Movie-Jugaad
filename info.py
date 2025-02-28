c357573ea438b2f23c56a45036cc0610fdc1d48d
Nikhil792 <114512871+Nikhil792@users.noreply.github.com>
2025-02-28 17:28:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -128,7 +128,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
 SHORTLINK_API = environ.get('SHORTLINK_API', '')
-#TUTORIAL = environ.get('TUTORIAL', 'https://t.me/newewwwwwwww/10') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/newewwwwwwww/10') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
 # Others
