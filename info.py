e0fb1ed2a145a975d2b1f4b21c64a9e13d3bd6fc
Nikhil792 <114512871+Nikhil792@users.noreply.github.com>
2025-04-13 20:25:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ PICS = (environ.get('PICS', 'https://envs.sh/e4E.jpg')).split()
 
 # Admins & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6667067260').split()] # For Multiple Id Use One Space Between Each.
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '6667067260 1799623305 6017903256').split()]  # For Multiple Id Use One Space Between Each.
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '6667067260 1799623305').split()]  # For Multiple Id Use One Space Between Each.
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
 # This Channel Is For When User Start Your Bot Then Bot Send That User Name And Id In This Log Channel, Same For Group Also.
