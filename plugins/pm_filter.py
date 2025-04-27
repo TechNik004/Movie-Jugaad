3eebfe82a4021b966009a8255fabde40863ed55c
TechNik004 <gorka8212@gmail.com>
2025-04-27 13:08:26 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2595,12 +2595,11 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
     temp.SHORT[message.from_user.id] = message.chat.id
     if settings["button"]:
        btn = [
-    {
-        'text': f"[{get_size(file['file_size'])}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file['file_name'].split()))}",
-        'callback_data': f'{pre}#{file["file_id"]}'
-    }
-    for file in files if "file_id" in file
-]
+            [
+                InlineKeyboardButton(
+                    text=f"[{get_size(file['file_size'])}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file['file_name'].split()))}", callback_data=f'{pre}#{file["file_id"]}'
+                ),
+            ]
         btn.insert(0, 
             [
                 InlineKeyboardButton(f'ǫᴜᴀʟɪᴛʏ', callback_data=f"qualities#{key}"),
